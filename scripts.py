b20fe1f2995f94aa1dafe0880da0d1f28968aff4
fraaays <202380114@psu.palawan.edu.ph>
2025-04-27 08:04:39 +0800

    second commit

--- scripts.py
+++ scripts.py
@@ -4,6 +4,10 @@ import socket
 Address = '127.0.0.1'
 Port = 8888
 
+for pin in range(0, 1000):
+    formatted_pin = f"{pin:03d}"
+    print(f"Testing PIN: {formatted_pin}")
+
 # Create socket connection
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((Address, Port))
